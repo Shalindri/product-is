ccd35781971c7bd7b88c75f5ac668e55e72c83b1
shalindri <shalindri20@gmail.com>
2021-10-25 07:30:44 +0530

    Address review comments

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/rest/api/user/authorized/apps/v2/ApplicationAuthorizedAppsNegativeTest.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/rest/api/user/authorized/apps/v2/ApplicationAuthorizedAppsNegativeTest.java
@@ -24,6 +24,7 @@ import org.apache.http.HttpStatus;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
@@ -44,6 +45,12 @@ public class ApplicationAuthorizedAppsNegativeTest extends UserAuthorizedAppsBas
 
         super.testInit(API_VERSION, swaggerDefinition, tenant);
         initUrls("me");
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void testInit() {
+
+        RestAssured.basePath = basePath;
     }
 
     @AfterMethod(alwaysRun = true)
